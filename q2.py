eb51a273849fe81b8b7e1f1552c67dae3a48f381
Michal Hoľa <hola2@uniba.sk>
2015-11-14 14:32:46 +0100

    added setup.cfg, split test for _adjacent_move_direction, added is_within_board and adjacent_spaces functions with tests

--- q2.py
+++ q2.py
@@ -1,9 +1,39 @@
+SIZE = 9
+MIN_POS = 0
+MAX_POS = SIZE - 1
+
 VERTICAL = (0, 1)
 HORIZONTAL = (1, 0)
 DIRECTIONS = (VERTICAL, HORIZONTAL)
 
+COL = 0
+ROW = 1
+
 
 def _adjacent_move_direction(from_, to):
-    direction = (abs(from_[0] - to[0]), abs(from_[1] - to[1]))
+    direction = (abs(from_[COL] - to[COL]), abs(from_[ROW] - to[ROW]))
     assert direction in DIRECTIONS, (from_, to)
     return direction
+
+
+def is_within_board(position):
+    return (
+        position[COL] >= MIN_POS and position[COL] <= MAX_POS
+    ) and (
+        position[ROW] >= MIN_POS and position[ROW] <= MAX_POS
+    )
+
+
+def adjacent_spaces(position):
+    assert is_within_board(position), position
+    spaces = []
+    if position[ROW] > MIN_POS:
+        spaces.append((position[COL], position[ROW] - 1))
+    if position[ROW] < MAX_POS:
+        spaces.append((position[COL], position[ROW] + 1))
+
+    if position[COL] > MIN_POS:
+        spaces.append((position[COL] - 1, position[ROW]))
+    if position[COL] < MAX_POS:
+        spaces.append((position[COL] + 1, position[ROW]))
+    return spaces
--- tests.py
+++ tests.py
@@ -1,11 +1,13 @@
 from nose.tools import (
-    assert_equal, raises,
+    assert_false, assert_equal, raises,
 )
 
 from q2 import (
     VERTICAL,
     HORIZONTAL,
     _adjacent_move_direction,
+    is_within_board,
+    adjacent_spaces,
 )
 
 
@@ -24,8 +26,49 @@ def test_adjacent_move_direction_wrong_3():
     _adjacent_move_direction([5, 2], [0, 2])
 
 
-def test_adjacent_move_direction_vertical():
+def test_adjacent_move_direction_vertical_1():
     assert_equal(_adjacent_move_direction([0, 0], [0, 1]), VERTICAL)
+
+
+def test_adjacent_move_direction_vertical_2():
     assert_equal(_adjacent_move_direction([5, 7], [5, 6]), VERTICAL)
+
+
+def test_adjacent_move_direction_horizontal_1():
     assert_equal(_adjacent_move_direction([3, 8], [2, 8]), HORIZONTAL)
+
+
+def test_adjacent_move_direction_horizontal_2():
     assert_equal(_adjacent_move_direction([7, 0], [8, 0]), HORIZONTAL)
+
+
+def test_is_within_board_wrong_1():
+    assert_false(is_within_board([-1, 0]))
+
+
+def test_is_within_board_wrong_2():
+    assert_false(is_within_board([1, 257]))
+
+
+def test_is_within_board_wrong_3():
+    assert_false(is_within_board([2147483649, -2147483649]))
+
+
+def test_adjacent_spaces_left_top_corner():
+    expected = set([(0, 1), (1, 0)])
+    assert_equal(set(adjacent_spaces((0, 0))), expected)
+
+
+def test_adjacent_spaces_right_bottom_corner():
+    expected = set([(8, 7), (7, 8)])
+    assert_equal(set(adjacent_spaces((8, 8))), expected)
+
+
+def test_adjacent_spaces_right_side():
+    expected = set([(8, 2), (8, 4), (7, 3)])
+    assert_equal(set(adjacent_spaces((8, 3))), expected)
+
+
+def test_adjacent_spaces_middle():
+    expected = set([(3, 4), (4, 3), (5, 4), (4, 5)])
+    assert_equal(set(adjacent_spaces((4, 4))), expected)
