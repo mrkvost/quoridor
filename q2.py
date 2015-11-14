4563854126c746c2cb0c2e96472e3d36eef57135
Michal Hoľa <hola2@uniba.sk>
2015-11-14 15:24:47 +0100

    refactoring, added wall_within_board with tests

--- q2.py
+++ q2.py
@@ -1,6 +1,8 @@
 SIZE = 9
-MIN_POS = 0
-MAX_POS = SIZE - 1
+MIN_PAWN_POS = 0
+MAX_PAWN_POS = SIZE - 1
+MIN_WALL_POS = 0
+MAX_WALL_POS = SIZE - 2
 
 VERTICAL = (0, 1)
 HORIZONTAL = (1, 0)
@@ -16,22 +18,28 @@ def _adjacent_move_direction(from_, to):
     return direction
 
 
-def pawn_within_board(pos):
-    return MIN_POS <= pos[COL] <= MAX_POS and (
-        MIN_POS <= pos[ROW] <= MAX_POS
+def pawn_within_board(position):
+    return MIN_PAWN_POS <= position[COL] <= MAX_PAWN_POS and (
+        MIN_PAWN_POS <= position[ROW] <= MAX_PAWN_POS
     )
 
 
-def adjacent_spaces(position):
+def adjacent_pawn_positions(position):
     assert pawn_within_board(position), position
     spaces = []
-    if position[ROW] > MIN_POS:
+    if position[ROW] > MIN_PAWN_POS:
         spaces.append((position[COL], position[ROW] - 1))
-    if position[ROW] < MAX_POS:
+    if position[ROW] < MAX_PAWN_POS:
         spaces.append((position[COL], position[ROW] + 1))
 
-    if position[COL] > MIN_POS:
+    if position[COL] > MIN_PAWN_POS:
         spaces.append((position[COL] - 1, position[ROW]))
-    if position[COL] < MAX_POS:
+    if position[COL] < MAX_PAWN_POS:
         spaces.append((position[COL] + 1, position[ROW]))
     return spaces
+
+
+def wall_within_board(position):
+    return MIN_WALL_POS <= position[COL] <= MAX_WALL_POS and (
+        MIN_WALL_POS <= position[ROW] <= MAX_WALL_POS
+    )
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from nose.tools import (
-    assert_false, assert_equal, raises,
+    assert_true, assert_false, assert_equal, raises,
 )
 
 from q2 import (
@@ -7,7 +7,8 @@ from q2 import (
     HORIZONTAL,
     _adjacent_move_direction,
     pawn_within_board,
-    adjacent_spaces,
+    adjacent_pawn_positions,
+    wall_within_board,
 )
 
 
@@ -54,21 +55,61 @@ def test_pawn_within_board_wrong_3():
     assert_false(pawn_within_board([2147483649, -2147483649]))
 
 
-def test_adjacent_spaces_left_top_corner():
+def test_pawn_within_board_correct_1():
+    assert_true(pawn_within_board([0, 0]))
+
+
+def test_pawn_within_board_correct_2():
+    assert_true(pawn_within_board([3, 7]))
+
+
+def test_pawn_within_board_correct_3():
+    assert_true(pawn_within_board([8, 8]))
+
+
+def test_adjacent_pawn_positions_left_top_corner():
     expected = set([(0, 1), (1, 0)])
-    assert_equal(set(adjacent_spaces((0, 0))), expected)
+    assert_equal(set(adjacent_pawn_positions((0, 0))), expected)
 
 
-def test_adjacent_spaces_right_bottom_corner():
+def test_adjacent_pawn_positions_right_bottom_corner():
     expected = set([(8, 7), (7, 8)])
-    assert_equal(set(adjacent_spaces((8, 8))), expected)
+    assert_equal(set(adjacent_pawn_positions((8, 8))), expected)
 
 
-def test_adjacent_spaces_right_side():
+def test_adjacent_pawn_positions_right_side():
     expected = set([(8, 2), (8, 4), (7, 3)])
-    assert_equal(set(adjacent_spaces((8, 3))), expected)
+    assert_equal(set(adjacent_pawn_positions((8, 3))), expected)
 
 
-def test_adjacent_spaces_middle():
+def test_adjacent_pawn_positions_middle():
     expected = set([(3, 4), (4, 3), (5, 4), (4, 5)])
-    assert_equal(set(adjacent_spaces((4, 4))), expected)
+    assert_equal(set(adjacent_pawn_positions((4, 4))), expected)
+
+
+def test_wall_within_board_wrong_1():
+    assert_false(wall_within_board([0, -1]))
+
+
+def test_wall_within_board_wrong_2():
+    assert_false(wall_within_board([65538, -1]))
+
+
+def test_wall_within_board_wrong_3():
+    assert_false(wall_within_board([-4294967296, 4294967296]))
+
+
+def test_wall_within_board_correct_1():
+    assert_true(pawn_within_board([0, 0]))
+
+
+def test_wall_within_board_correct_2():
+    assert_true(pawn_within_board([0, 7]))
+
+
+def test_wall_within_board_correct_3():
+    assert_true(pawn_within_board([5, 4]))
+
+
+def test_wall_within_board_correct_4():
+    assert_true(pawn_within_board([7, 7]))
