b52b826516dea0d739be4e3a7a58aa7b0e1410bd
Michal Hoľa <hola2@uniba.sk>
2016-03-24 13:49:44 +0100

    added test db, refactored db tests

--- quoridor/tests/test_db.py
+++ quoridor/tests/test_db.py
@@ -21,17 +21,32 @@ from quoridor.ai import MLMCPerceptron
 
 LIST_TABLES_SQL = "SELECT name FROM sqlite_master WHERE type='table'"
 
-import time
+
+@nottest
+def with_path_or_tempfile(file_path=None, **temfile_kwargs):
+    def decorator(function):
+        @functools.wraps(function)
+        def wrapper(*args, **kwargs):
+            if file_path is None:
+                with tempfile.NamedTemporaryFile(**temfile_kwargs) as db_file:
+                    return function(db_file.name, *args, **kwargs)
+            else:
+                return function(file_path, *args, **kwargs)
+        return wrapper
+    return decorator
 
 
 @nottest
-def with_temp_db_and_session(function):
-    @functools.wraps(function)
-    def wrapper(*args, **kwargs):
-        with tempfile.NamedTemporaryFile(suffix='.test.db') as db_file:
-            with closing(make_db_session(db_file.name)) as db_session:
-                return function(db_file, db_session, *args, **kwargs)
-    return wrapper
+def with_db_session(file_path=None):
+    def decorator(function):
+        @functools.wraps(function)
+        @with_path_or_tempfile(file_path=file_path, suffix='.test.db')
+        def wrapper(db_file_path, *args, **kwargs):
+            with closing(make_db_session(db_file_path)) as db_session:
+                setattr(db_session, '__dbfilename__', db_file_path)
+                return function(db_session, *args, **kwargs)
+        return wrapper
+    return decorator
 
 
 @nottest
@@ -41,48 +56,94 @@ def get_all_table_names(db_session):
     )
 
 
+@nottest
+def assert_perceptron_equal(perceptron, expected_attributes):
+    assert_equal(perceptron.alpha, expected_attributes['alpha'])
+    assert_equal(perceptron.momentum, expected_attributes['momentum'])
+    assert_equal(
+        perceptron.out_sigmoided,
+        expected_attributes['out_sigmoided']
+    )
+
+    assert_equal(
+        len(perceptron.weights),
+        len(expected_attributes['weights'])
+    )
+    for i, layer in enumerate(expected_attributes['weights']):
+        assert_true(numpy.array_equal(perceptron.weights[i], layer))
+
+
 @attr('db', 'build_db')
-@with_temp_db_and_session
-def test_build_db(db_file, db_session):
-    build_db(db_file.name)
+@with_db_session()
+def test_build_db(db_session):
+    build_db(db_session.__dbfilename__)
     assert_equal(get_all_table_names(db_session), ['network', 'weight'])
     assert_equal(len(db_session.query(Network).all()), 0)
     assert_equal(len(db_session.query(Weight).all()), 0)
 
 
 @attr('db', 'save')
-@with_temp_db_and_session
-def test_save_weights(db_file, db_session):
-    perceptron = MLMCPerceptron([2, 2, 2])
-    build_db(db_file.name)
-    db_save_network(db_session, perceptron, name='2_2_2')
-    # TODO: assert .. db_session.query(Weight).count() ... etc.
+@with_db_session()
+def test_save_network(db_session):
+    attributes = {
+        'alpha': 0.1,
+        'momentum': 0.8,
+        'out_sigmoided': True,
+        'weights': [
+            numpy.array([
+                [1, 2, 3],
+                [4, 5, 6],
+            ]),
+            numpy.array([
+                [7, 8, 9],
+            ]),
+        ],
+    }
+
+    perceptron = MLMCPerceptron(**attributes)
+    build_db(db_session.__dbfilename__)
+    db_save_network(db_session, perceptron, name='2_2_1')
+
+    network = db_session.query(Network).filter_by(name='2_2_1').one()
+    assert_equal(network.alpha, attributes['alpha'])
+    assert_equal(network.momentum, attributes['momentum'])
+    assert_equal(network.out_sigmoided, attributes['out_sigmoided'])
+
+    db_weights = db_session.query(Weight)\
+        .filter_by(network_id=network.id).all()
+    expected_weights_count = sum([l.size for l in attributes['weights']])
+    assert_equal(len(db_weights), expected_weights_count)
+
+    for weight in db_weights:
+        assert_equal(
+            weight.weight,
+            attributes['weights'][weight.layer][weight.output][weight.input]
+        )
 
 
 @attr('db', 'load')
-def test_load_weights():
-    DB_PATH = 'quoridor/tests/data/saved.db'
-    with closing(make_db_session(DB_PATH)) as db_session:
-        network_attributes = db_load_network(db_session, '2_3_4')
+@with_db_session('quoridor/tests/data/saved.db')
+def test_load_network_1(db_session):
+    network_attributes = db_load_network(db_session, '2_3_4')
     perceptron = MLMCPerceptron(**network_attributes)
-
-    assert_equal(perceptron.alpha, 0.1)
-    assert_equal(perceptron.momentum, 0.8)
-    assert_true(perceptron.out_sigmoided)
-
-    assert_equal(len(perceptron.weights), 2)
-    expected_weights = [
-        numpy.array([
-            [0.1, 0.2, 0.3],
-            [0.4, 0.5, 0.6],
-            [0.7, 0.8, 0.9],
-        ]),
-        numpy.array([
-            [ 0.0, -0.1, -0.2, -0.3],
-            [-0.4, -0.5, -0.6, -0.7],
-            [-0.8, -0.9, -1.0, -1.1],
-            [-1.2, -1.3, -1.4, -1.5],
-        ]),
-    ]
-    for i, layer in enumerate(expected_weights):
-        assert_true(numpy.array_equal(perceptron.weights[i], layer))
+    assert_perceptron_equal(
+        perceptron,
+        {
+            'alpha': 0.1,
+            'momentum': 0.8,
+            'out_sigmoided': True,
+            'weights': [
+                numpy.array([
+                    [0.1, 0.2, 0.3],
+                    [0.4, 0.5, 0.6],
+                    [0.7, 0.8, 0.9],
+                ]),
+                numpy.array([
+                    [ 0.0, -0.1, -0.2, -0.3],
+                    [-0.4, -0.5, -0.6, -0.7],
+                    [-0.8, -0.9, -1.0, -1.1],
+                    [-1.2, -1.3, -1.4, -1.5],
+                ]),
+            ],
+        }
+    )
