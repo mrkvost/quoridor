d32859ec0118bf14c988acd6eedefd0aa12177d2
Michal Hoľa <hola2@uniba.sk>
2016-04-17 13:48:44 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,45 +1,82 @@
 import sys
+import pip
 
 from setuptools import setup, find_packages
 
 
-setup_info = dict(
-    name="quoridor",
-    version="0.1.0",
-    author="Michal Hola",
-    author_email="hola2@uniba.sk",
-    url="https://github.com/mrkvost/quoridor",
-    description=(
-        "Board game Quoridor for two players with Neural Network as opponent."
-    ),
+REQUIREMENTS = {
+    'nose': '1.3.7',
+    'numpy': '1.10.4',
+    'SQLAlchemy': '1.0.12',
+}
 
-    packages=find_packages(),
-    setup_requires=[
-        'nose==1.3.7',
-        'numpy==1.10.4',
-        'SQLAlchemy==1.0.12',
-    ],
-    entry_points={
-        'console_scripts': [
-            'qc = quoridor.quoridor:main',
-            'train = quoridor.training:run',
-        ],
-    },
-)
 
-if 'install_requires' not in setup_info:
-    setup_info['install_requires'] = []
+def get_setup_requirements():
+    matching_installed_packages = dict([
+        (package.name.lower(), package)
+        for package in pip.get_installed_distributions()
+        if package.project_name in REQUIREMENTS
+    ])
+    # ...TODO...
 
-if 'develop' in sys.argv or 'install' in sys.argv:
-    setup_info['install_requires'].append('pip==8.1.0')
-    setup_info['install_requires'].append('numpy==1.10.4')
-    setup_info['install_requires'].append('SQLAlchemy==1.0.12')
+    setup_requirements = []
+    for package_name, required_version in REQUIREMENTS.items():
+        if package_name.lower() in matching_installed_packages:
+            installed = matching_installed_packages[package_name.lower()]
+            if installed.version < required_version:
+                warning = (
+                    'Warning, package {name} is already installed but version '
+                    'is lower than required one {current} < {required}. If'
+                    'any program functionality fails, try to upgrade this '
+                    'package.'
+                ).format(
+                    name=package_name,
+                    current=installed.version,
+                    required=required_version,
+                )
+                print warning
+        else:
+            setup_requirements.append(
+                '=='.join([package_name, required_version])
+            )
+    return setup_requirements
 
-if 'develop' in sys.argv:
-    setup_info['install_requires'].append('latex==0.6.1')
 
-    if 'scripts' not in setup_info:
-        setup_info['scripts'] = []
-    setup_info['scripts'].append('bin/docmake')
+def create_setup_info():
+    setup_requirements = get_setup_requirements()
+    setup_info = dict(
+        name="quoridor",
+        version="0.1.0",
+        author="Michal Hola",
+        author_email="hola2@uniba.sk",
+        url="https://github.com/mrkvost/quoridor",
+        description=(
+            "Board game Quoridor for two players with Neural Network as opponent."
+        ),
 
-setup(**setup_info)
+        packages=find_packages(),
+        setup_requires=setup_requires,
+        entry_points={
+            'console_scripts': [
+                'qc = quoridor.quoridor:main',
+                'train = quoridor.training:run',
+            ],
+        },
+    )
+
+    if 'install_requires' not in setup_info:
+        setup_info['install_requires'] = []
+
+    if 'develop' in sys.argv or 'install' in sys.argv:
+        setup_info['install_requires'] = list(set(
+            set(setup_info['install_requires'] + setup_requirements
+        ))
+
+    if 'develop' in sys.argv:
+        setup_info['install_requires'].append('latex==0.6.1')
+
+        if 'scripts' not in setup_info:
+            setup_info['scripts'] = []
+        setup_info['scripts'].append('bin/docmake')
+
+setup(**create_setup_info)
