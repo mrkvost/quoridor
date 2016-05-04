87a8503798ac74df9444a8e9c911033ea887acd9
Michal Hoľa <hola2@uniba.sk>
2016-05-04 20:23:20 +0200

    updated script for peto

--- quoridor/peto.py
+++ quoridor/peto.py
@@ -1,28 +1,66 @@
-import random
+from time import clock
+start = clock()
 
-from core.game import YELLOW, GREEN, Quoridor2, InvalidMove
+from core.game import YELLOW, GREEN, Quoridor2
 from core.context import QuoridorContext
-from ai.players import HeuristicPlayer, QlearningNetworkPlayer
+from ai.players import HeuristicPlayer
+
+
+OUTPUT_FMT = """\
+      YELLOW     GREEN
+{yellow:>12} vs. {green}
+{yw:>7} wins     {gw} wins
+
+            TOTAL
+         games: {game_count}
+    avg. moves: {avg_moves:.3f}
+  init seconds: {init_seconds:.4f}
+  play seconds: {seconds:.4f}
+
+            RATE
+  seconds/game: {sg:.4f}
+  games/second: {gs:.2f}\
+"""
 
 
 def run():
     game = Quoridor2()
     context = QuoridorContext(game)
+    heuristic = HeuristicPlayer(game)
     players = {
-        YELLOW: {'name': 'qlnn', 'player': QlearningNetworkPlayer(game)},
-        GREEN: {'name': 'heuristic', 'player': HeuristicPlayer(game)},
+        YELLOW: {'name': 'heuristic', 'player': heuristic},
+        GREEN: {'name': 'heuristic', 'player': heuristic},
     }
-    context.reset(players=players)
-    print context
-    while not context.is_terminal:
+    game_count = 500
+    yellow_wins = 0
+    green_wins = 0
+    avg_moves = 0.0
+
+    play_start = clock()
+
+    for n in xrange(1, game_count + 1):
+        context.reset(players=players)
+        while not context.is_terminal:
+            heuristic.play(context)
         if context.state[0] == YELLOW:
-            actions = tuple(game.all_actions)
-            for action in random.sample(actions, len(actions)):
-                try:
-                    context.update(action)
-                    break
-                except InvalidMove:
-                    continue
+            green_wins += 1
         else:
-            players[GREEN]['player'](context)
-        print context
+            yellow_wins += 1
+        avg_moves += (len(context.history) - avg_moves) / n
+
+    end = clock()
+
+    play_seconds = end - play_start
+    init_seconds = play_start - start
+    print OUTPUT_FMT.format(
+        yellow=players[YELLOW]['name'],
+        green=players[GREEN]['name'],
+        game_count=game_count,
+        avg_moves=avg_moves,
+        init_seconds=init_seconds,
+        seconds=play_seconds,
+        sg=float(play_seconds) / game_count,
+        gs=float(game_count) / play_seconds,
+        yw=yellow_wins,
+        gw=green_wins,
+    )
